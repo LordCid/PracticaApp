c7a35a3af186b5eb25477eb7af73f0d4d5d5038b
ALBERTPORTATIL\Albert <cidalbert@hotmail.com>
2020-07-26 16:01:40 +0200

    Main list adapter added

---
+++ app/src/main/java/com/slashmobility/seleccion/albert/cid/presentation/main/MainListAdapter.kt
@@ -0,0 +1,31 @@
+package com.slashmobility.seleccion.albert.cid.presentation.main
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.slashmobility.seleccion.albert.cid.domain.model.Group
+import kotlin.properties.Delegates
+
+class MainListAdapter: RecyclerView.Adapter<ListItemViewHolder>() {
+    var groupList: List<Group> by Delegates.observable(emptyList()) { _, oldValue, newValue ->
+        if (oldValue != newValue) {
+            notifyDataSetChanged()
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListItemViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ListItemViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+}
+
+class ListItemViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+
+}
