80097bc81e8593c06e0c2447763889eed3347165
ALBERTPORTATIL\Albert <cidalbert@hotmail.com>
2020-07-26 16:01:58 +0200

    Domain-view model added

---
+++ app/src/main/java/com/slashmobility/seleccion/albert/cid/domain/model/Group.kt
@@ -0,0 +1,10 @@
+package com.slashmobility.seleccion.albert.cid.domain.model
+
+data class Group (
+    val id: Int,
+    val name: String,
+    val description: String,
+    val descriptionShort: String,
+    val defaultImageUrl: String,
+    val dateLong: Long
+)
