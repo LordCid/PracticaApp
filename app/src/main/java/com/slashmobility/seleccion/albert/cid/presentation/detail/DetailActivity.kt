675474177e629954f0cca2ed9a7b53c552a9429f
ALBERTPORTATIL\Albert <cidalbert@hotmail.com>
2020-07-26 22:14:27 +0200

    detail activity

---
+++ app/src/main/java/com/slashmobility/seleccion/albert/cid/presentation/detail/DetailActivity.kt
@@ -0,0 +1,11 @@
+package com.slashmobility.seleccion.albert.cid.presentation.detail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.detail)
+    }
+}
--- app/src/main/java/com/slashmobility/seleccion/albert/cid/presentation/main/MainActivity.kt
+++ app/src/main/java/com/slashmobility/seleccion/albert/cid/presentation/main/MainActivity.kt
@@ -4,15 +4,38 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import androidx.core.os.bundleOf
+import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.slashmobility.seleccion.albert.cid.R
+import com.slashmobility.seleccion.albert.cid.domain.model.Group
+import com.slashmobility.seleccion.albert.cid.domain.usecase.GetGroupsUseCaseImpl
+import com.slashmobility.seleccion.albert.cid.presentation.main.state.MainViewState
+import com.xpertai.test.domain.imageloader.GlideImplementation
+import kotlinx.android.synthetic.main.fragment_list.*
+import kotlinx.coroutines.Dispatchers
 
 const val GROUP_ID = "GROUP_ID"
 class MainActivity : AppCompatActivity() {
 
+    private val imagesLoader = GlideImplementation()
+
+    private lateinit var groupAdapter: GroupListAdapter
+
+    private lateinit var viewModel: MainListViewModelImpl
+
+    private val viewModelFactory = MainListViewModelFactory(GetGroupsUseCaseImpl(), Dispatchers.IO)
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        groupAdapter = GroupListAdapter(imagesLoader)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.toolbar))
+        viewModel = ViewModelProviders.of(this, viewModelFactory)[MainListViewModelImpl::class.java]
+        setUpUI()
 //        supportActionBar?.setDisplayHomeAsUpEnabled(true)
 //        supportActionBar?.setDisplayShowHomeEnabled(true)
     }
@@ -32,4 +55,36 @@ class MainActivity : AppCompatActivity() {
             else -> super.onOptionsItemSelected(item)
         }
     }
+    override fun onResume() {
+        super.onResume()
+        viewModel.mainViewState.observe(::getLifecycle, ::updateUI)
+        viewModel.getGroups()
+    }
+
+    private fun setUpUI() {
+        listView.apply {
+            layoutManager = LinearLayoutManager(context, RecyclerView.VERTICAL, false)
+            this.adapter = groupAdapter
+        }
+
+        groupAdapter.onClickItem = {
+            val bundle = bundleOf(GROUP_ID to it)
+//            findNavController().navigate(R.id.action_List_to_Detail, bundle)
+        }
+    }
+
+    private fun updateUI(screenState: MainViewState) {
+        when (screenState) {
+            is MainViewState.Loading -> showLoadingDialogFragment()
+            is MainViewState.ShowFullData -> showGroups(screenState.groups)
+            is MainViewState.ShowFavorites -> showGroups(screenState.groups)
+            is MainViewState.Error -> showErrorDialogFragment()
+        }
+    }
+
+    private fun showGroups(groups: List<Group>) { groupAdapter.groupList = groups.toList() }
+
+    private fun showLoadingDialogFragment() {}
+
+    private fun showErrorDialogFragment() {}
 }
