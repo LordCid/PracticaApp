caa9e5ff2efbcb9aa840735a673819297f80286c
ALBERTPORTATIL\Albert <cidalbert@hotmail.com>
2020-09-09 19:08:58 +0200

    MainActivity Test

--- app/src/androidTest/java/com/slashmobility/seleccion/albert/cid/presentation/main/MainActivityTest.kt
+++ app/src/androidTest/java/com/slashmobility/seleccion/albert/cid/presentation/main/MainActivityTest.kt
@@ -1,18 +1,25 @@
 package com.slashmobility.seleccion.albert.cid.presentation.main
 
 import android.content.Intent
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModelProvider
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
 import org.junit.Before
 import androidx.test.rule.ActivityTestRule
-import com.slashmobility.seleccion.albert.cid.di.AppComponentFactory
-import com.slashmobility.seleccion.albert.cid.di.TestAppComponent
+import com.nhaarman.mockitokotlin2.given
+import com.nhaarman.mockitokotlin2.mock
 import com.slashmobility.seleccion.albert.cid.di.TestAppComponentFactory
 import com.slashmobility.seleccion.albert.cid.domain.App
+import com.slashmobility.seleccion.albert.cid.presentation.main.state.MainViewState
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.mockito.Mock
 
 @RunWith(AndroidJUnit4::class)
 class MainActivityTest {
@@ -21,7 +28,10 @@ class MainActivityTest {
     @JvmField
     val activityRule = ActivityTestRule(MainActivity::class.java, true, false)
 
-    private lateinit var viewModel: ViewModelProvider.NewInstanceFactory
+    @Mock
+    private lateinit var viewModelFactory: MainListViewModelFactory
+    private val viewModel = mock<MainListViewModelImpl>()
+    private val liveDataViewState = mock<MutableLiveData<MainViewState>>()
 
     @Before
     fun setUp() {
@@ -30,7 +40,9 @@ class MainActivityTest {
         val appComponent = TestAppComponentFactory.create(app)
         appComponent.inject(app)
 
-        viewModel = appComponent.getViewModelFactory()
+        viewModelFactory = appComponent.getViewModelFactory() as MainListViewModelFactory
+
+        stubViewModelFactory()
 
         val intent = Intent(
             InstrumentationRegistry.getInstrumentation()
@@ -40,8 +52,22 @@ class MainActivityTest {
         activityRule.launchActivity(intent)
     }
 
+
+
     @Test
     fun WhenErrorStateShowErrorDialog() {
+        givenErrorState()
 
+        onView(withText("Error al descargar grupos")).check(matches(isDisplayed()))
+    }
+
+    private fun stubViewModelFactory() {
+       given(viewModelFactory.create(MainListViewModelImpl::class.java)).willReturn(viewModel)
+    }
+
+    private fun givenErrorState() {
+        liveDataViewState.value = MainViewState.Error
+        given(viewModel.mainViewState).willReturn(liveDataViewState)
+        given(viewModel.getGroups()).willAnswer { viewModel.mainViewState }
     }
 }
--- app/src/main/java/com/slashmobility/seleccion/albert/cid/di/TestMainListModule.kt
+++ app/src/main/java/com/slashmobility/seleccion/albert/cid/di/TestMainListModule.kt
@@ -2,11 +2,12 @@ package com.slashmobility.seleccion.albert.cid.di
 
 import androidx.lifecycle.ViewModelProvider
 import com.nhaarman.mockitokotlin2.mock
+import com.slashmobility.seleccion.albert.cid.presentation.main.MainListViewModelFactory
 import dagger.Module
 import dagger.Provides
 
 @Module
 class TestMainListModule {
     @Provides
-    fun provideMockMainVierModelFactory() =  mock<ViewModelProvider.NewInstanceFactory>()
+    fun provideMockMainViewModelFactory(): ViewModelProvider.NewInstanceFactory =  mock<MainListViewModelFactory>()
 }
